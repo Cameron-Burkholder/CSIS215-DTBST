fc32e7d4219013723d96710a5c17e6fc90dcabd9
Cameron-Burkholder <cburkholder19@gmail.com>
2020-09-23 19:11:51 -0400

    09-23-2020 19:11 - Initialize repository.

---
+++ Binary_Search_Tree/BSTNode.h
@@ -0,0 +1,60 @@
+// From the software distribution accompanying the textbook
+// "A Practical Introduction to Data Structures and Algorithm Analysis,
+// Third Edition (C++)" by Clifford A. Shaffer.
+// Source code Copyright (C) 2007-2011 by Clifford A. Shaffer.
+
+// This is the file to include for access to the complete binary node
+// template implementation
+
+#include "book.h"
+#include "BinNode.h"
+
+#ifndef BSTNODE_H
+#define BSTNODE_H
+
+// Simple binary tree node implementation
+template <typename Key, typename E>
+class BSTNode : public BinNode<E> {
+private:
+  Key k;                  // The node's key
+  E it;                   // The node's value
+  BSTNode* lc;            // Pointer to left child
+  BSTNode* rc;            // Pointer to right child
+  bool leftBit : 1;       // For both these variables, true will mean that it is a thread whereas false will mean
+  bool rightBit : 1;      // that it points to a node (not inorder thread)
+
+public:
+  // Two constructors -- with and without initial values
+  BSTNode() { lc = rc = NULL; }
+  BSTNode(Key K, E e, BSTNode* l =NULL, BSTNode* r =NULL)
+    { k = K; it = e; lc = l; rc = r; }
+  ~BSTNode() {}             // Destructor
+
+  // Functions to set and return the value and key
+  E& element() { return it; }
+  void setElement(const E& e) { it = e; }
+  Key& key() { return k; }
+  void setKey(const Key& K) { k = K; }
+
+  // Functions to set and return the children
+  inline BSTNode* left() const { return lc; }
+  void setLeft(BinNode<E>* b) { lc = (BSTNode*)b; }
+  inline BSTNode* right() const { return rc; }
+  void setRight(BinNode<E>* b) { rc = (BSTNode*)b; }
+
+  /* CAMERON BURKHOLDER */
+  // Functions to get and set status of left and right thread bitfields 
+  inline int leftIsThread() const { return leftBit;  }
+  void setLeftBit(bool bit : 1) {
+      leftBit = bit;
+  }
+  inline int rightIsThread() const { return rightBit; }
+  void setRightBit(bool bit : 1) {
+      rightBit = bit;
+  }
+
+  // Return true if it is a leaf, false otherwise
+  bool isLeaf() { return (lc == NULL) && (rc == NULL); }
+};
+
+#endif
---
+++ Binary_Search_Tree/Binary_Search_Tree/main.cpp
@@ -0,0 +1,11 @@
+// Binary_Search_Tree.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Author: Cameron Burkholder
+// Date: 09-21-2020
+
+#include <iostream>
+#include "../BST.h"
+
+int main()
+{
+    std::cout << "Cameron Burkholder - LAB 2: Double-Threaded BST" << std::endl;
+}
